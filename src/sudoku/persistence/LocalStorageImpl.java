c4ca5a70b21b8ceef3e053876c1a7c54626cf2b7
Colin MacRury <colinmacrury99@gmail.com>
2020-09-27 15:58:16 +0100

    Singleton added in LocalStorageImpl

--- src/sudoku/persistence/LocalStorageImpl.java
+++ src/sudoku/persistence/LocalStorageImpl.java
@@ -6,16 +6,24 @@ import sudoku.problemdomain.SudokuGame;
 import java.io.*;
 
 public class LocalStorageImpl implements IStorage {
-    private static File GAME_DATA = new File(
-            System.getProperty("user.home"),
-            "gamedata.txt"
-    );
 
+    private static File GAME_DATA;
+
+    private static File GetLocalStorageFileInstance() {
+        if (GAME_DATA != null) {
+            return GAME_DATA;
+
+        } else {
+            GAME_DATA = new File(System.getProperty("user.home"), "gamedata.txt");
+            return GAME_DATA;
+        }
+
+    }
 
     @Override
     public void updateGameData(SudokuGame game) throws IOException {
         try {
-            FileOutputStream fileOutputStream = new FileOutputStream(GAME_DATA);
+            FileOutputStream fileOutputStream = new FileOutputStream(GetLocalStorageFileInstance());
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
             objectOutputStream.writeObject(game);
             objectOutputStream.close();
@@ -26,7 +34,7 @@ public class LocalStorageImpl implements IStorage {
 
     @Override
     public SudokuGame getGameData() throws IOException {
-        FileInputStream fileInputStream = new FileInputStream(GAME_DATA);
+        FileInputStream fileInputStream = new FileInputStream(GetLocalStorageFileInstance());
         ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
         try {
             SudokuGame gameState = (SudokuGame) objectInputStream.readObject();
